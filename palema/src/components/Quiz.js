96ac55b073d295a2b479ab1ed13d368189411c43
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-04-05 18:51:16 +0100

    New Questions Added

--- palema/src/components/Quiz.js
+++ palema/src/components/Quiz.js
@@ -1,13 +1,46 @@
-import React , { useContext } from 'react';
+import React , { useState, useContext } from 'react';
 import { QuizContext } from "../helpers/Context";
+import { Questions } from "../helpers/QuestionBank";
 import "../App.css";
 
 function Quiz(){
+
     const { gameState, setGameState } = useContext(QuizContext);
 
+    const [currentQuestion, setCurrQuestion] = useState(0);
+    const [optionChosen, setOptionChosen] = useState("1");
+
+    const nextQuestion = () => {
+        setCurrQuestion(currentQuestion + 1);
+    };
+
+    const finishQuiz = () => {
+        setGameState("endScreen")
+    }
     return (
         <div className="Quiz">
-            QUEEEZ 
+            <div className="options">
+                <h1>{Questions[currentQuestion].prompt}</h1>
+                <button onClick ={() => setOptionChosen("A")}>
+                    {Questions[currentQuestion].optionA} 
+                </button>
+                <button onClick ={() => setOptionChosen("B")}> 
+                    {Questions[currentQuestion].optionB} 
+                </button>
+                <button onClick ={() => setOptionChosen("C")}> 
+                    {Questions[currentQuestion].optionC} 
+                </button>
+                <button onClick ={() => setOptionChosen("D")}> 
+                    {Questions[currentQuestion].optionD} 
+                </button>
+            </div>
+
+            {currentQuestion == Questions.length -1 ? (
+            <button  onClick={finishQuiz}> Finish Quiz </button>
+            ) : (
+            <button onClick={nextQuestion}> Next Question</button>
+            )} 
+            
             <button className ="quitButton" onClick={() => {
                 setGameState("menu");
             }}>QUIT</button>
