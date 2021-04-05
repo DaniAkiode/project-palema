3aa11f4133020731ebc34f023ed51bcbce2de0d6
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-04-05 11:18:04 +0100

    New Compoents added

--- palema/src/App.js
+++ palema/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useContext} from 'react';
+import MainMenu from "./components/MainMenu";
+import HomePage from "./components/HomePage";
+import Quiz from "./components/Quiz";
+import EndQuiz from "./components/EndQuiz";
+import ChatBot from "./components/chatbot";
+
+import { QuizContext } from './helpers/Context'
+
 
 function App() {
+  const [gameState, setGameState] = useState("homepage");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Palema</h1>
+      <QuizContext.Provider value={{gameState, setGameState}}>
+      {gameState === "homepage"  && <HomePage />}
+      {gameState === "menu"  && <MainMenu />}
+      {gameState === "quiz"  && <Quiz />}
+      {gameState === "endquiz"  && <EndQuiz />}
+      {gameState === "ChatBot"  && <ChatBot />}
+      </QuizContext.Provider>
     </div>
   );
 }
